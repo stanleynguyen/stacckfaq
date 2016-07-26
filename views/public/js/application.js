3e7ce463d5fc8636badabb9ce5857dca7f5121c6
Stanley Nguyen <hung.ngn.the@gmail.com>
2016-07-26 05:11:11 +0000

    fix small bugs

--- views/public/js/application.js
+++ views/public/js/application.js
@@ -19,8 +19,7 @@ $(document).ready(function() {
                 setTimeout(function() {
                     $('.alert-success').hide();
                 }, 5000);
-                console.log(newFaq)
-                var htmlBody = '<div class="jumbotron" id="' + newFaq.id + '">\
+                var htmlBody = '<div class="jumbotron" id="' + newFaq._id + '">\
                     <form>\
                       <fieldset class="form-group">\
                         <label for="question">Question</label>\
@@ -37,6 +36,7 @@ $(document).ready(function() {
                     </form>\
                 </div>';
                 $('#faq').append(htmlBody);
+                bindButtonsEvent();
             } else {
                 setTimeout(function(){
                     $('.alert-danger').show();
@@ -46,62 +46,64 @@ $(document).ready(function() {
             $('.alert-danger').show();
         });
     });
-    
-    $('#faq form .btn-primary').click(function() {
-        $(this).hide();
-        $(this).siblings('.btn-success').show();
-        $(this).siblings('.btn-default').show();
-        $(this).siblings('.form-group').children('.form-control').prop('disabled', false);
-    });
-    
-    $('#faq form .btn-default').click(function() {
-        $(this).hide();
-        $(this).siblings('.btn-success').hide();
-        $(this).siblings('.btn-primary').show();
-        $(this).siblings('.form-group').children('.form-control').prop('disabled', true);
-    });
-    
-    $('#faq form .btn-danger').click(function() {
-        var id = $(this).parent().parent().attr('id');
-        console.log(id);
-        $.ajax({
-            url: '/api',
-            type: 'DELETE',
-            data: {id: id}
-        }).done(function(message) {
-            if (message === 'success'){
-                $('#' + id).remove();
-            } else {
-                alert('database error! please try again!');
-            }
-        }).fail(function() {
-            alert('fail to delete! please try again!');
+    function bindButtonsEvent() {
+        $('#faq form .btn-primary').click(function() {
+            $(this).hide();
+            $(this).siblings('.btn-success').show();
+            $(this).siblings('.btn-default').show();
+            $(this).siblings('.form-group').children('.form-control').prop('disabled', false);
         });
-    });
-    
-    $('#faq form').submit(function() {
-        event.preventDefault();
-        var $this = $(this);
-        var data = {
-            id: $(this).parent().attr('id'),
-            question: $(this).find('.question').val(),
-            answer: $(this).find('.answer').val()
-        };
-        $.ajax({
-            url: '/api',
-            type: 'PUT',
-            data: data
-        }).done(function(message) {
-            if (message === 'success') {
-                $this.find('.btn-success').hide();
-                $this.find('.btn-default').hide();
-                $this.find('.btn-primary').show();
-                $this.find('.form-control').prop('disabled', true);
-            } else {
-                alert('database error! please try again!');
-            }
-        }).fail(function() {
-            alert('fail to update! please try again!');
+        
+        $('#faq form .btn-default').click(function() {
+            $(this).hide();
+            $(this).siblings('.btn-success').hide();
+            $(this).siblings('.btn-primary').show();
+            $(this).siblings('.form-group').children('.form-control').prop('disabled', true);
         });
-    });
+        
+        $('#faq form .btn-danger').click(function() {
+            var id = $(this).parent().parent().attr('id');
+            console.log(id);
+            $.ajax({
+                url: '/api',
+                type: 'DELETE',
+                data: {id: id}
+            }).done(function(message) {
+                if (message === 'success'){
+                    $('#' + id).remove();
+                } else {
+                    alert('database error! please try again!');
+                }
+            }).fail(function() {
+                alert('fail to delete! please try again!');
+            });
+        });
+        
+        $('#faq form').submit(function() {
+            event.preventDefault();
+            var $this = $(this);
+            var data = {
+                id: $(this).parent().attr('id'),
+                question: $(this).find('.question').val(),
+                answer: $(this).find('.answer').val()
+            };
+            $.ajax({
+                url: '/api',
+                type: 'PUT',
+                data: data
+            }).done(function(message) {
+                if (message === 'success') {
+                    $this.find('.btn-success').hide();
+                    $this.find('.btn-default').hide();
+                    $this.find('.btn-primary').show();
+                    $this.find('.form-control').prop('disabled', true);
+                } else {
+                    alert('database error! please try again!');
+                }
+            }).fail(function() {
+                alert('fail to update! please try again!');
+            });
+        });
+    }
+    bindButtonsEvent();
 });
